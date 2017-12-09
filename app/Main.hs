c8cfa68af0152faad689042b957c6f568addb9d7
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-09 14:11:37 +0900

    チャプター1実装完了

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
+import Data.Tiger
 import Lib
 
+prog =
+    CompoundStm
+        (
+            AssignStm 
+                "a" 
+                (OpExp (NumExp 5) Plus (NumExp 3)) 
+        )
+        (
+            CompoundStm
+                (
+                    AssignStm 
+                        "b" 
+                        (
+                            EseqExp 
+                                (PrintStm [IdExp "a",OpExp (IdExp "a") Minus (NumExp 1)])
+                                (OpExp (NumExp 10) Times (IdExp "a"))
+                        )
+                )
+                ( PrintStm [IdExp "b"])
+        )
+    
 main :: IO ()
-main = someFunc
+main = print prog
---
+++ src/Data/Tiger.hs
@@ -0,0 +1,18 @@
+module Data.Tiger where
+
+type Id =String
+
+data Binop =Plus | Minus | Times | Div deriving Show
+
+data Stm = 
+    CompoundStm Stm Stm
+    | AssignStm Id Exp
+    | PrintStm [Exp] 
+    deriving Show
+
+data Exp =
+    IdExp Id
+    | NumExp Int
+    | OpExp Exp Binop Exp
+    | EseqExp Stm Exp 
+    deriving Show
