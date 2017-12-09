15265e2f4165745a04d5b8f46467f9eef64c29e4
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-09 15:51:01 +0900

    テスト追加

--- test/Spec.hs
+++ test/Spec.hs
@@ -16,5 +16,11 @@ executeExpTest = TestList
         "executeExp test 2" ~: 
             executeExp (OpExp (NumExp 4) Plus (NumExp 8)) ~?= 12 ,
         "executeExp test 3" ~: 
+            executeExp (OpExp (NumExp 4) Minus (NumExp 8)) ~?= -4 ,
+        "executeExp test 4" ~: 
+            executeExp (OpExp (NumExp 4) Times (NumExp 8)) ~?= 32 ,
+        "executeExp test 5" ~: 
+            executeExp (OpExp (NumExp 16) Div (NumExp 8)) ~?= 2 ,
+        "executeExp test 6" ~: 
             executeExp ( EseqExp (PrintStm [(NumExp 114)]) (NumExp 7) ) ~?= 7
     ]
