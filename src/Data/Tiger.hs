8606951100ccccbfca0917beb6b500f6be46ea6d
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-09 16:35:32 +0900

    executeExpをStateモナド化した

--- src/Data/Tiger.hs
+++ src/Data/Tiger.hs
@@ -1,13 +1,19 @@
 module Data.Tiger where
 
 import qualified Data.Map.Strict as Map
+import Control.Monad.State
 
 testMap :: Map.Map Char Int
 testMap = Map.singleton 'v' 45
 
 type Id =String
 
-data Binop =Plus | Minus | Times | Div deriving Show
+type VariableMap = Map.Map Id Int
+
+emptyVariableMap :: VariableMap
+emptyVariableMap = Map.empty::VariableMap
+
+data Binop = Plus | Minus | Times | Div deriving Show
 
 data Stm = 
     CompoundStm Stm Stm
@@ -23,10 +29,22 @@ data Exp =
     deriving Show
 
 --式を実行して結果を得る
-executeExp :: Exp -> Int
-executeExp (NumExp i) = i
-executeExp (OpExp e1 Plus e2) = (executeExp e1)+(executeExp e2) 
-executeExp (OpExp e1 Minus e2) = (executeExp e1)-(executeExp e2) 
-executeExp (OpExp e1 Times e2) = (executeExp e1)*(executeExp e2) 
-executeExp (OpExp e1 Div e2) = (executeExp e1) `div` (executeExp e2) 
+executeExp :: Exp -> State VariableMap Int
+executeExp (NumExp i) = return i
+executeExp (OpExp e1 Plus e2) = do 
+    v1 <- (executeExp e1) 
+    v2 <- (executeExp e2)
+    return $ v1 + v2  
+executeExp (OpExp e1 Minus e2) = do 
+    v1 <- (executeExp e1) 
+    v2 <- (executeExp e2)
+    return $ v1 - v2   
+executeExp (OpExp e1 Times e2) = do 
+    v1 <- (executeExp e1) 
+    v2 <- (executeExp e2)
+    return $ v1 * v2   
+executeExp (OpExp e1 Div e2) = do 
+    v1 <- (executeExp e1) 
+    v2 <- (executeExp e2)
+    return $ v1 `div` v2  
 executeExp (EseqExp _ e) = executeExp e
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 import Test.HUnit
 import Data.Tiger
+import Control.Monad.State
 
 main :: IO ()
 main =do 
@@ -9,18 +10,21 @@ main =do
         ]
     return ()
 
+executeExpTestHelper :: Exp->Int
+executeExpTestHelper e = evalState (executeExp e) emptyVariableMap
+
 executeExpTest :: Test
 executeExpTest = TestList 
     [
-        "executeExp test 1" ~: executeExp (NumExp 4) ~?= 4 ,
+        "executeExp test 1" ~: executeExpTestHelper (NumExp 4) ~?= 4 ,
         "executeExp test 2" ~: 
-            executeExp (OpExp (NumExp 4) Plus (NumExp 8)) ~?= 12 ,
+            executeExpTestHelper (OpExp (NumExp 4) Plus (NumExp 8)) ~?= 12 ,
         "executeExp test 3" ~: 
-            executeExp (OpExp (NumExp 4) Minus (NumExp 8)) ~?= -4 ,
+            executeExpTestHelper (OpExp (NumExp 4) Minus (NumExp 8)) ~?= -4 ,
         "executeExp test 4" ~: 
-            executeExp (OpExp (NumExp 4) Times (NumExp 8)) ~?= 32 ,
+            executeExpTestHelper (OpExp (NumExp 4) Times (NumExp 8)) ~?= 32 ,
         "executeExp test 5" ~: 
-            executeExp (OpExp (NumExp 16) Div (NumExp 8)) ~?= 2 ,
+            executeExpTestHelper (OpExp (NumExp 16) Div (NumExp 8)) ~?= 2 ,
         "executeExp test 6" ~: 
-            executeExp ( EseqExp (PrintStm [(NumExp 114)]) (NumExp 7) ) ~?= 7
+            executeExpTestHelper ( EseqExp (PrintStm [(NumExp 114)]) (NumExp 7) ) ~?= 7
     ]
