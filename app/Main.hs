7e5c3f8f594dc214e819405988df75c3abb8e46a
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-12 13:11:10 +0900

    build時のワーニングを消すためにコードを修正

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,29 @@
 module Main where
 
 import           Data.Tiger
-import           Lib
 
-prog =
-    CompoundStm
-        (
-            AssignStm
-                "a"
-                (OpExp (NumExp 5) Plus (NumExp 3))
-        )
-        (
-            CompoundStm
-                (
-                    AssignStm
-                        "b"
-                        (
-                            EseqExp
-                                (PrintStm [IdExp "a",OpExp (IdExp "a") Minus (NumExp 1)])
-                                (OpExp (NumExp 10) Times (IdExp "a"))
-                        )
-                )
-                ( PrintStm [IdExp "b"])
-        )
 
 main :: IO ()
-main = runStm prog
+main =
+    runStm prog
+    where prog =
+            CompoundStm
+            (
+                AssignStm
+                    "a"
+                    (OpExp (NumExp 5) Plus (NumExp 3))
+            )
+            (
+                CompoundStm
+                    (
+                        AssignStm
+                            "b"
+                            (
+                                EseqExp
+                                    (PrintStm [IdExp "a",OpExp (IdExp "a") Minus (NumExp 1)])
+                                    (OpExp (NumExp 10) Times (IdExp "a"))
+                            )
+                    )
+                    ( PrintStm [IdExp "b"])
+            )
+
