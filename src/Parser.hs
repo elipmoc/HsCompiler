d95bf60c6581fdb3c16052f8e8f3ccd4436e7189
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-16 20:06:38 +0900

    テスト通った

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,11 +1,12 @@
 module Parser where
 
-import           Control.Applicative    ((*>), (<$>), (<*), (<*>))
+import           Control.Applicative    ((*>), (<$>), (<*), (<*>), (<|>))
+import           Control.Monad
 import           Interpreter
 import           Text.Parsec.Char       (char, digit, letter)
 import           Text.Parsec.Combinator (many1)
 import           Text.Parsec.Error      (ParseError)
-import           Text.Parsec.Prim       (parse)
+import           Text.Parsec.Prim       (parse, try)
 import           Text.Parsec.String     (Parser)
 
 --パーサの簡易実行と出力をする関数
@@ -21,12 +22,41 @@ parseStm :: String -> Either ParseError Stm
 parseStm input = parse parserAssignStm "" input
 
 parserAssignStm :: Parser Stm
-parserAssignStm = AssignStm <$> parserId <* char '=' <*> parserNumExp
+parserAssignStm = AssignStm <$> parserId <* char '=' <*> parserPlusMinusExp
 
 parserNumExp ::Parser Exp
 parserNumExp = do
     numStr <- many1 digit
-    return $ NumExp(read numStr::Int)
+    return $ NumExp (read numStr::Int)
+
+parserPlusMinusExp :: Parser Exp
+parserPlusMinusExp =
+    try(do
+        a <- parserTimesDivExp
+        b <- parserPlusMinusBinop
+        c <- parserPlusMinusExp
+        return $ OpExp a b c)
+    <|> parserTimesDivExp
+
+parserPlusMinusBinop :: Parser Binop
+parserPlusMinusBinop =
+    (char '+' >> return Plus)
+    <|> (char '-' >> return Minus)
+
+
+parserTimesDivExp :: Parser Exp
+parserTimesDivExp =
+    try(do
+        a <- parserNumExp
+        b <- parserTimesDivBinop
+        c <- parserTimesDivExp
+        return $ OpExp a b c)
+    <|> parserNumExp
+
+parserTimesDivBinop :: Parser Binop
+parserTimesDivBinop =
+    (char '*' >> return Times)
+    <|> (char '/' >> return Div)
 
 parserId :: Parser Id
 parserId =  many1 letter
